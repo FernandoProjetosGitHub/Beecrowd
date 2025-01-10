cb3ac5916e36b7dd92fd6c208bdf69fd70277f96
imjustablueghost <p4cote@gmail.com>
2025-01-10 14:30:37 -0300

    Atualização de resolução dos exercícios.

--- 1003.js
+++ 1003.js
@@ -1,7 +1,7 @@
-let lines = `30
-10`.split('\n');
+let lines = `0
+0`.split('\n');
 let A = parseInt(lines[0])
 let B = parseInt(lines[1])
 
-const SOMA = ( A + B);
-console.log(SOMA);
+const resultado = (A + B);
+console.log(`SOMA = ${resultado}`);
