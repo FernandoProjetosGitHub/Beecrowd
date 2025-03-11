afe8e3937be55642ab28a711cec6aad709d2043d
FernandoProjetosGitHub <fernandoaugustoprojetos@gmail.com>
2025-03-11 18:10:57 -0300

    Adicionado resolução do exercício 1115

---
+++ Desafios/Iniciante/1115.js
@@ -0,0 +1,28 @@
+let lines = `2 2
+3 -2
+-8 -1
+-7 1
+0 2`.split('\n');
+
+for(let splitter = 0; splitter < lines.length; splitter ++){
+
+    let linesSplitted = lines[splitter].split(" ");
+
+    let lines0 = parseInt(linesSplitted[0]);
+    let lines1 = parseInt(linesSplitted[1]);
+
+    if(lines0 === 0 || lines1 === 0){
+        break;
+    }else if(lines0 > 0 && lines1 > 0){
+        console.log("primeiro");
+        
+    }else if(lines0 < 0 && lines1 > 0){
+        console.log("segundo");
+        
+    }else if(lines0 < 0 && lines1 < 0) {
+        console.log("terceiro");
+        
+    }else if(lines0 > 0 && lines1 < 0){
+        console.log("quarto");
+    }
+}
