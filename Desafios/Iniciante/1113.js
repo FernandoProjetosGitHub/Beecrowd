274b305fc93b9e3b2bd36753dd81bf66213049eb
FernandoProjetosGitHub <fernandoaugustoprojetos@gmail.com>
2025-03-11 15:03:57 -0300

    Adicionado resolução do exercício 1113

---
+++ Desafios/Iniciante/1113.js
@@ -0,0 +1,26 @@
+let lines = `5 4
+7 2
+3 8
+2 2`.split('\n');
+
+for(let splitter = 0; splitter < lines.length; splitter ++){
+
+    let linesSplitted = lines[splitter].split(" ");
+    let lines0 = parseInt(linesSplitted[0]);
+    let lines1 = parseInt(linesSplitted[1]);
+
+    if(lines0 === lines1){
+
+        break;
+
+    }else if(lines0 > lines1){
+
+        console.log(`Decrescente`);
+        
+    }else{
+
+        console.log(`Crescente`);
+        
+    }
+    
+}
