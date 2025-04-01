2c9287176a394b288dbacd0c00660c5db8fe1ff6
FernandoProjetosGitHub <fernandoaugustoprojetos@gmail.com>
2025-04-01 14:42:02 -0300

    Adicionado resolução do exercício 1156

--- Desafios/Iniciante/1156.js
+++ Desafios/Iniciante/1156.js
@@ -1,12 +1,33 @@
+// Regra do de cima: i x 2 + 1;
+// exemplo: 
+// para i  = 0, resulta em : 0 x 2 + 1 = 1;
+// para i = 1, resulta em : 1 x 2 + 1 = 3;
 
 
-let s = 0
-for(let i = 1/2; i <= 18; i++ ){
-    console.log(i)
-    s += 1/i
+// Regra do de baixo: 2 ^ i ;
+// exemplo:
+// para i = 1, resulta em :  2 ^ 1 = 2;
+// para i = 2, resulta em :  2 ^ 2 = 4;
+// para i = 3, resulta em :  2 ^ 3 = 8;
+
+let resultado = 0;
+
+for(let index = 0; index < 20; index ++){
+
+    let numerador =  index * 2 + 1;
+    let denominador = Math.pow(2, index);
+    resultado += numerador / denominador;
+    
+    
 }
+console.log(resultado.toFixed(2));
 
-// function sequenciaS2 () {
-//     //caso de parada
-//     if()
-// }
+// value = 1
+// index = 0
+// for
+// multiplicador = 0
+// 1
+// index 1
+
+
+
